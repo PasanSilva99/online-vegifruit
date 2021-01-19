474323e2db705cfed6c390a2c20d5fd90275b4f0
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2021-01-19 16:08:17 +0530

    staff Dashboard changed

--- Vegifruit Part 1/staffDashboard.aspx.cs
+++ Vegifruit Part 1/staffDashboard.aspx.cs
@@ -9,9 +9,39 @@ namespace Vegifruit_Part_1
 {
     public partial class staffDashboard : System.Web.UI.Page
     {
+
+        String DisplayName = "";        
+        
+        /// <summary> 
+        /// Che wether the session is null.
+        /// </summary>
+        /// <param name="sessid"></param>
+        /// <returns></returns>
+        protected bool checkSession(String sessid)
+        {
+            if (Session[sessid] != null && Session[sessid].ToString().Length > 0)
+                return true;
+
+            return false;
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+            /// get the username from the saved cookie. If it is not exist, Redirect to the Login Page.
+            if (checkSession("Fullname"))
+            {
+                DisplayName = Session["Fullname"].ToString();
+                lbl_DisplayName.Text = DisplayName;  //Assign the text to the label
+            }
+            else
+                Response.Redirect("Login?message=Please%20Login%20First", false);
+
+                
+
         }
+
+
     }
 }
--- Vegifruit Part 1/staffDashboard.aspx.designer.cs
+++ Vegifruit Part 1/staffDashboard.aspx.designer.cs
@@ -42,6 +42,15 @@ namespace Vegifruit_Part_1
         protected global::System.Web.UI.WebControls.TextBox tb_search2;
 
         /// <summary>
+        /// lbl_DisplayName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_DisplayName;
+
+        /// <summary>
         /// btn1 control.
         /// </summary>
         /// <remarks>
